f76cefa00eb5dcbd27f54532c34717f66890110a
sgoutouly <sgoutouly@gmail.com>
2017-10-12 00:07:01 +0200

    Polishing de la version async opérationnelle

--- src/main/java/fr/laposte/disfe/cicsproxy/proxy/CommareaProxy.java
+++ src/main/java/fr/laposte/disfe/cicsproxy/proxy/CommareaProxy.java
@@ -1,7 +1,6 @@
 
 package fr.laposte.disfe.cicsproxy.proxy;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 
 import javax.annotation.PostConstruct;
@@ -11,7 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.stereotype.Component;
 
 import com.couchbase.client.java.Bucket;
@@ -22,7 +20,6 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.logging.LogLevel;
-import io.reactivex.netty.client.ConnectionRequest;
 import io.reactivex.netty.protocol.tcp.client.TcpClient;
 import io.reactivex.netty.protocol.tcp.server.TcpServer;
 import lombok.RequiredArgsConstructor;
@@ -36,8 +33,6 @@ import rx.Observable;
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public final class CommareaProxy {
 
-	
-
 	private static final Logger LOG = LoggerFactory.getLogger(CommareaProxy.class);
 	
 	@Value("${proxy.local.port}")
@@ -48,9 +43,8 @@ public final class CommareaProxy {
     private int remotePort;
 	
 	/** Modes de fonctionnement */
-	private enum ProxyMode { IDLE, RECORD, REPLAY, RECORD_IF_EMPTY }
+	private enum ProxyMode { IDLE, RECORD, RECORD_IF_EMPTY, MOCK }
 	
-	/** Charset de travail */
 	public static final Charset CHARSET = Charset.forName("UTF-8");
 	public static final Charset CICS_CHARSET = Charset.forName("IBM01147");
 	
@@ -62,56 +56,60 @@ public final class CommareaProxy {
 
 		LOG.info("CICS Proxying *:" + LOCAL_PORT + " ...");
 
-        /* Starts a new HTTP server on an ephemeral port which acts as a proxy to the target server started above.*/
         TcpServer.newServer(LOCAL_PORT)
         	.enableWireLogging("proxy-server", LogLevel.DEBUG)
         	.start(serverConn -> {
+        		return serverConn.getInput()
+        			.replayable()
+        			.flatMap(question -> { 
+	    				return this.bucket.async()
+	    					.get(toBase64(question))
+	    					.switchIfEmpty(forward(question)
+	    						.map(reponse -> saveExchange(question, reponse))
+	    						.flatMap(remoteResponse -> serverConn.writeAndFlushOnEach(Observable.just(remoteResponse)))
+	    						.cast(JsonDocument.class)) // Ceci permet de faire passer ce bloc pour un JsonDocument
+	    					.flatMap(doc -> {
+	    						LOG.info(" => Utilisation du cache ...");
+	        					final byte[] reponse = new Base64().decode(doc.content().getString("reponse"));
+	        					return serverConn.writeAndFlushOnEach(Observable.just(Unpooled.copiedBuffer(reponse)));
+	    					});
+	    				});
         		
-        		final Observable<ByteBuf> stream = serverConn.getInput().replayable();
-        		
-        		return stream.flatMap(buf -> { 
-    				String question64 =  new String(new Base64().encode(ByteBufUtil.getBytes(buf)), CHARSET);
-    				return this.bucket.async()
-    					.get(question64)
-    					.switchIfEmpty(Observable.error(new RuntimeException("Cache vide !")))
-    					.onErrorResumeNext(e -> {
-    						LOG.info(" => Appel du serveur distant ..");
-        					Observable<ByteBuf> resp = TcpClient.newClient(remoteHost, remotePort)
-        							.createConnectionRequest()
-        							.flatMap(clientConn -> clientConn.writeAndFlushOnEach(stream)
-        								.cast(ByteBuf.class) 
-        								.mergeWith(clientConn.getInput()))
-        								.map(r -> save(buf, r));
-        					
-            				return serverConn.writeAndFlushOnEach(resp)
-            						.cast(JsonDocument.class);
-    					})
-    					.flatMap(doc -> {
-    						LOG.info(" => Utilisation du cache ...");
-        					byte[] reponse = new Base64().decode(doc.content().getString("reponse"));
-        					ByteBuf reponseBB = Unpooled.copiedBuffer(reponse);
-        					return serverConn.writeAndFlushOnEach(Observable.just(reponseBB));
-    					});
-    				});
-    				
-        		//});
         	})
-        .awaitShutdown();
-
+        	.awaitShutdown();
 	}
 	
+	/**
+	 * Appel le serveur distant
+	 * @param serverConn
+	 * @param q
+	 * @return Observable<Void>
+	 */
+	Observable<ByteBuf> forward(ByteBuf q) {
+		LOG.info(" => Appel du serveur distant ..");
+		return TcpClient.newClient(this.remoteHost, this.remotePort)
+			.createConnectionRequest()
+			.flatMap(clientConn -> clientConn.writeAndFlushOnEach(Observable.just(q))
+				.cast(ByteBuf.class) 
+				.mergeWith(clientConn.getInput()));
+	}
 	
-	public ByteBuf save(ByteBuf q, ByteBuf r) {
+	/**
+	 * @param q
+	 * @param r
+	 * @return
+	 */
+	public ByteBuf saveExchange(ByteBuf q, ByteBuf r) {
 		try {
 	     	LOG.info("Enregistrement de la réponse en cache ...");
 	    	
-	     	final String question = new String(new Base64().encode(ByteBufUtil.getBytes(q)), CHARSET);
-	    	this.bucket.insert(JsonDocument.create(question, 
+	     	final String question = toBase64(q);
+	    	this.bucket.upsert(JsonDocument.create(question, 
 	    		JsonObject.create()
-	    			.put("questionDecoded", new String(ByteBufUtil.getBytes(q), CICS_CHARSET))
+	    			.put("questionDecoded", decodeCics(q))
 	    			.put("question", question)
-	    			.put("reponseDecoded", new String(ByteBufUtil.getBytes(r), CICS_CHARSET))
-	    			.put("reponse", new String((new Base64()).encode(ByteBufUtil.getBytes(r)), CHARSET)))
+	    			.put("reponseDecoded", decodeCics(r))
+	    			.put("reponse", toBase64(r)))
 	    	);
 	    	LOG.info("Enregistrement réussi !");
 	    	
@@ -122,6 +120,13 @@ public final class CommareaProxy {
 		}
 	}
 	
+	private String decodeCics(final ByteBuf buf) {
+		return new String(ByteBufUtil.getBytes(buf), CICS_CHARSET);
+	}
+	
+	private String toBase64(final ByteBuf buf) {
+		return new String(new Base64().encode(ByteBufUtil.getBytes(buf)), CHARSET);
+	}
 	
 	private static void safeDump(byte[] b) {
 		LOG.info(new String(b, CICS_CHARSET));
