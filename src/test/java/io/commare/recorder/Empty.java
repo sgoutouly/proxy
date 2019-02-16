32013738183c109a5cacb269285425826408aa89
sgoutouly <sgoutouly@gmail.com>
2019-02-16 09:36:36 +0100

    Add Jacoco support

---
+++ src/test/java/io/commare/recorder/Empty.java
@@ -0,0 +1,19 @@
+package io.commare.recorder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Empty
+ * 16 Feb 2019
+ */
+public class Empty {
+
+	@Test
+	public void should() {
+		assertTrue(true);
+	}
+
+}
